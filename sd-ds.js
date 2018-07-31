b6f38de3c03881a3030847746e71be917b7142da
Yago Huayta <yago.huayta@outlook.com>
2018-07-31 16:53:50 -0500

    cambio de img logrado

---
+++ sd-ds.js
@@ -0,0 +1,8 @@
+"use strict"
+
+
+function cambiarimg(){
+    var gato = document.getElementById("gato");
+    gato.innerHTML = "<div id='gato'><img id='gato1' src='/Users/developer/Desktop/sd.jpg' title='foto de gato'/></div>";
+    console.log("xxxxx");
+}
