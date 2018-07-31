f9fe528800fc19da59d2bb3e234c60dc45d0cecb
Yago Huayta <yago.huayta@outlook.com>
2018-07-31 11:49:11 -0500

    una nueva fncion

--- 30-eventos.js
+++ 30-eventos.js
@@ -5,36 +5,39 @@
 
 //Eventos de raton
 
-var boton = document.querySelector("#boton");
+window.addEventListener('load', function(){
+    var boton = document.querySelector("#boton");
 
-function cambiarColor(){
-    console.log("Me has dado click");
-    var bg = boton.style.background;
-    if(bg == "green"){
-        boton.style.background = "red";
-    }else{
-        boton.style.background = "green";
+    function cambiarColor(){
+        console.log("Me has dado click");
+        var bg = boton.style.background;
+        if(bg == "green"){
+            boton.style.background = "red";
+        }else{
+            boton.style.background = "green";
+        }
+    
+        return true;
     }
-
-    return true;
-}
-boton.addEventListener('mouseout',function(){
-    boton.style.background = '#CCC';
+    boton.addEventListener('mouseout',function(){
+        boton.style.background = '#CCC';
+    });
+    
+    // Focus
+    var input = document.querySelector("#campo_nombre")
+    
+    input.addEventListener('focus', function(){
+        console.log("Estas dentro del input");
+    })
+    
+    // Blur
+    input.addEventListener('blur', function(){
+        console.log("Estas fuera del input");
+    })
+    
+    // Keydown
+    input.addEventListener('keydown', function(event){
+        console.log("Estas pulsando esta tecla ", String.fromCharCode(event.keyCode));
+    });
 });
 
-// Focus
-var input = document.querySelector("#campo_nombre")
-
-input.addEventListener('focus', function(){
-    console.log("Estas dentro del input");
-})
-
-// Blur
-input.addEventListener('blur', function(){
-    console.log("Estas fuera del input");
-})
-
-// Keydown
-input.addEventListener('keydown', function(event){
-    console.log("Estas pulsando esta tecla ", String.fromCharCode(event.keyCode));
-});
