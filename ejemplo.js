36db72e838e5f494f928df2052238d4edd81ffd0
Yago Huayta <yago.huayta@outlook.com>
2018-08-01 19:30:19 -0500

    d

---
+++ ejemplo.js
@@ -0,0 +1,8 @@
+"use strict"
+var numero1 = parseInt(prompt("introduce un numero", 0));
+
+if(numero1%2 != 0){
+    console.log(" El "+ numero1 + " es impar ")
+}else{
+    console.log("El "+numero1+" es par")
+}
