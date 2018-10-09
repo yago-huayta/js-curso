2a9335f5f5e1d54b460f35fd1739cf7ad04b98fa
Yago Huayta <yago.huayta@outlook.com>
2018-10-09 00:47:33 -0500

    json

---
+++ 32-json.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// JSON = JavaScript Object Notation
+
+var pelicula = {
+    titulo: "Batman vs Superman",
+    year: 2017,
+    pais: "Estados Unidos"
+};
+
+var peliculas = [
+    {titulo:"la verdad", year:"2016", pais:"asdfvsdfv"},
+    pelicula
+];
+
+var index;
+for(index in peliculas){
+    
+}
+
+console.log(pelicula);
