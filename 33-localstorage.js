8dd74939a310aa8b7f19cc25f46fa9a9e9c2f0fa
Yago Huayta <yago.huayta@outlook.com>
2018-10-10 00:26:11 -0500

    d

--- 33-localstorage.js
+++ 33-localstorage.js
@@ -1,1 +1,10 @@
 "use strict"
+
+// Localstorage
+
+if(typeof(Storage) !== 'undefined'){
+    console.log("localstorage disponible");
+}else{
+    console.log("localstorage no compatible");
+}
+console.log("sahbcaksbx");
