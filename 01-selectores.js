d53bde507b4f2800bb0f5a8c495e725df7d751f7
Yago Huayta <yago.huayta@outlook.com>
2018-11-10 13:21:45 -0500

    colores y selectores css

--- 01-selectores.js
+++ 01-selectores.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 
     var rojo = $("#rojo").css("color", "red")
-                         .css("background", "blue");
+                         .css("background", "lightblue");
     
- 
+    var amarillo = $("#amarillo").css("color","yellow")
+                         .css("background", "lightblue");
+
+     var verde = $("#verde").css("color", "green")
+                         .css("background", "lightblue");
 
 });
