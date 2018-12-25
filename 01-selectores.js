fd7402e5103695c4c9a96831ff5db0e6e71df7b8
Yago Huayta <yago.huayta@outlook.com>
2018-12-25 16:38:44 -0500

    jq

--- 01-selectores.js
+++ 01-selectores.js
@@ -1,16 +1,36 @@
 $(document).ready(function(){
-
-    var rojo = $("#rojo").css("color", "red")
-                         .css("background", "lightblue");
     
-    var amarillo = $("#amarillo").css("color","yellow")
-                         .css("background", "lightblue");
 
-     var verde = $("#verde").css("color", "green")
-                         .css("background", "lightblue");
+   //Selector de ID (Como su nombre dice selecciona atraves del ID)
+    //(el ";" colocalo en el ultimo comando por que si no es
+    //como si se cerrara antes y te aparecera un error posdata:hasme caso Yago
+    // del futuro :V XDd)
 
-//Selectores de clases 
+    var red = $("#rojo").css("background", "red")
+                         .css("color", "white");   
 
-    $('.cebra')
 
+    var yellow = $('#amarillo').css("background", "yellow")
+                                 .css("color", "blue");                                
+
+    var green = $('#verde').css("background", "green")
+                           .css("color", "white");                        
+
+
+    /*
+     Selector de clase: (Selecciona a traves del class, se usa para
+     identificar un conjunto de etiquetas y modificarlos 
+     en cambio el ID es para 1 solo
+     como un DNI para la persona pero para identificar a todos
+     seria humanos)
+    */
+
+    var mi_clase = $('.zebra')
+
+    $('.borde_oculto').click(function(){
+        
+        console.log("Click dado :v")
+        
+        $(this).addClass('zebra');
+    });
 });
