b8aff149ccfb00869870e15b05611871effb6a00
Yago Huayta <yago.huayta@outlook.com>
2020-03-19 09:45:18 -0500

    repaso

--- 02-variables.js
+++ 02-variables.js
@@ -2,9 +2,14 @@
 
 //VARIABLES
 //Una es un contenedor de informacion
+//Todos los texto van con comillas
+//Siempre tratar de que el nobre de la variable este relacionado con el contenido.
+//Se pueden concatenar variables para mostrarlas juntas en un alert asi:
 
 var pais = "Peru";
 var continente = "América"
 var antiguedad = "2018" 
+
+var pais_continente_antiguedad = pais + '' + continente + '' + antiguedad;
 
 console.log (pais, continente, antiguedad);
