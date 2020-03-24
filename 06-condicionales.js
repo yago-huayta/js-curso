a8e7dc84bc2fccdaf918b6a3edbc98cb3840d847
Yago Huayta <yago.huayta@outlook.com>
2020-03-24 08:25:04 -0500

    repaso

--- 06-condicionales.js
+++ 06-condicionales.js
@@ -11,8 +11,8 @@ var nombre = 'Yago Huayta';
     igual =
     Diferente !=
 */
-if(edad <= 18){
-    console.log(nombre+" tiene "+edad+" años es mayor de edad ")
+if(edad >= 18){
+    console.log(nombre+" tiene "+edad+" ,años es mayor de edad ")
 if(edad <= 33){
     console.log("Todavia eres millenial");}
 else if (edad >= 70)
