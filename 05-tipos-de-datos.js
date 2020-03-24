55fc6d8194304f133eabcffec6fd662006626a18
Yago Huayta <yago.huayta@outlook.com>
2020-03-24 08:09:52 -0500

    repaso

--- 05-tipos-de-datos.js
+++ 05-tipos-de-datos.js
@@ -8,4 +8,10 @@ alert("Es resultado de la operacion es: "+operacion)
 
 //Tipos de datos
 var numero_entero = 44;
-var cadena_de_texto = "Hola mundo JavaScript"
+var cadena_de_texto = "Hola mundo JavaScript";
+var numero_falso = "12" ;
+var verdadero_o_falso = false;
+
+// Con el comando typeof podemos saber si la varible es un numero o texto
+//por ejemplo 
+//console.log(typeof numero_entero)
